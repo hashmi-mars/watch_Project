0f9bb1303a190ccb175085fec722c16bb2551931
AbuBakar <hashmi.mars@gmail.com>
2022-02-11 14:38:13 +0500

    quick_Update
    full working Clock
    tested
    It is working fine

--- watch_project1.py
+++ watch_project1.py
@@ -1,8 +1,20 @@
+from cProfile import label
+from cgitb import text
 from tkinter import *
 from tkinter.ttk import *
 
 from time import strftime
 
 root = Tk()
-root.title("Bakar Watch")
+root.title("HASHMI CLOCK")
 
+def time():
+    string=strftime("%H:%M:%S: %p")
+    label.config(text=string)
+    label.after(1000, time)
+    
+label = Label(root, font=("Algerian", 120), background="blue", foreground="red")
+
+label.pack(anchor="center")
+time()
+mainloop()
